ec4db6e86d5fbd9c60f41a9d6e164e136b3f970a
tommercoder <tomashevskiysergiy@gmail.com>
2020-04-09 21:01:09 +0300

    11

--- Level1.Designer.cs
+++ Level1.Designer.cs
@@ -49,7 +49,6 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = global::Project.Properties.Resources.level1background;
             this.ClientSize = new System.Drawing.Size(800, 600);
             this.Controls.Add(this.exit_level1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
